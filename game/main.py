3357f7bc5e0ede53787b8b3a2083713da9f7f284
Tobias <tobias.vinblad0013@stud.hkr.se>
2023-02-15 21:48:22 +0100

    created files

--- game/main.py
+++ game/main.py
@@ -5,22 +5,20 @@
 Lets play a game of "Guess my number".
 
 I will be thinking af a number between 1 and 100.
-You shall try to guess the number.
+You shall try to game the number.
 
-I will let you know if your guess is lower or higher than
+I will let you know if your game is lower or higher than
 the number that I am thinking of.
 
 You have 5 guesses. Then you loose.
 
 """
 
-import shell
-
 
 def main():
     """Execute the main program."""
     print(__doc__)
-    shell.Shell().cmdloop()
+    return
 
 
 if __name__ == "__main__":
