de8f35cc0f896e1552b5759a26de5e340f59d173
Eddie <eddie.gustafsson@stud.hkr.se>
2023-03-06 14:32:07 +0100

    made unittest for Player class

--- test/test_player.py
+++ test/test_player.py
@@ -4,10 +4,29 @@
 """Unit testing."""
 
 import unittest
+from war import player
 
 class TestGameClass(unittest.TestCase):
     """Test the class."""
 
     def test_init_default_object(self):
         """Instantiate an object and check its properties."""
-        pass
+        exp = player.Player("a", [1, 2, 3, 4], False)
+        self.assertIsInstance(exp, player.Player)
+    
+    def test_play_card(self):
+        exp = player.Player("a", [1, 2, 3, 4], False)
+        ref = [exp, exp, exp, exp]
+
+        ref[0].play_card()
+        ref[0].play_card()
+        self.assertEqual(len(ref), len(ref))
+
+    def test_clearing_hand_stack(self):
+        exp = player.Player("Eddie", [1,2,3], ['a', 'b', 'c'])
+        ref = [exp, exp, exp, exp]
+
+        ref[0].clear_hand()
+        ref[0].clear_stack()
+
+        self.assertEqual(ref[0].hand_empty(), ref[0].stack_empty())
