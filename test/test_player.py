43388a491721fb322cdf356dc89e2969d2ca79d8
Eddie <eddie.gustafsson@stud.hkr.se>
2023-03-06 11:35:44 +0100

    Something

---
+++ test/test_player.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Unit testing."""
+
+import unittest
+
+class TestGameClass(unittest.TestCase):
+    """Test the class."""
+
+    def test_init_default_object(self):
+        """Instantiate an object and check its properties."""
+        pass
