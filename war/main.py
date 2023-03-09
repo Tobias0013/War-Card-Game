ae4652231b331d92a3e96cb60a5f4906e274768c
Eddie <eddie.gustafsson@stud.hkr.se>
2023-03-09 14:30:47 +0100

    Made rules in docstrings in main.py

--- war/main.py
+++ war/main.py
@@ -10,7 +10,13 @@ the one with the highest card value, wins the cards
 
 The game will end when one of the players has an empty hand.
 
---- Skriv spelregler ---
+The deck is divided evenly among the players. Player plays one card each round to a stack
+and the one with highest card value wins both cards and takes them to their hand.
+If both players card have the same value then a war begins. Then both players play 2 more cards,
+one unknown and one known. when 
+
+This repeats until one player has won all cards from the other player.
+
 
 Type in 'start' to begin a game. Good luck!
 
